a1e54a35a0ed79667de7334a6c2368f725a6cba3
ohanyere <mooref068@gmail.com>
2022-09-26 20:51:32 -0700

    data structures

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        first_character = None
+    else:
+        first_character = sentence[0]
+    return (len(sentence), first_character)
